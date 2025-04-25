c30b373fcd9ee4ae2c56345a2d1c6761ffd8de04
Katherine Brock <48523663+kbrock91@users.noreply.github.com>
2025-04-25 12:09:53 -0500

    Update api_link_new_git_repo.py

--- api_link_new_git_repo.py
+++ api_link_new_git_repo.py
@@ -1,8 +1,7 @@
 import requests
 
 # Configuration
-NEW_GIT_ORG_REPO= '<org>/<repo>' #e.g. 'kbrock91/dbt-workspace-scripts'
-API_BASE_URL = 'https://cloud.getdbt.com/api/v3'
+API_BASE_URL = 'https://cloud.getdbt.com/api/v3' #update to match your dbt Cloud account API Base URL
 DBT_CLOUD_ACCOUNT_ID = ''
 DBT_CLOUD_PROJECT_ID = ''
 DBT_CLOUD_API_TOKEN = ''
@@ -14,7 +13,8 @@ GITHUB_INSTALLATION_ID = ''
 # Alternatively, you can go to the page https://<dbt_cloud_url>/api/v2/integrations/github/installations/ and read the value of id or use the http provider to retrieve it automatically like in the example below.
 
 
-REMOTE_URL = f'git://github.com/{NEW_GIT_ORG_REPO}.git'
+current_github_org= ''
+new_github_org = ''
 
 HEADERS = {
     'Authorization': f'Token {DBT_CLOUD_API_TOKEN}',
@@ -28,13 +28,14 @@ def get_current_project_info(dbt_cloud_project_id):
     response = requests.get(url, headers=HEADERS)
 
     if response.status_code in (200, 201):
-        print(f"✅ Project fetched successfully. name: {response.json()['data']['name']}, repo_id: {response.json()['data'].get('repository_id')}")
-        return response.json()['data']['name'], response.json()['data'].get('repository_id')
+        print(f"✅ Project fetched successfully. name: {response.json()['data']['name']}, repo_id: {response.json()['data'].get('repository_id')}, repo_name:  {response.json()['data']['repository'].get('github_repo')}")
+        return response.json()['data']['name'], response.json()['data'].get('repository_id'), response.json()['data']['repository'].get('github_repo')
     else:
         raise Exception(f"❌ Error fetching project: {response.status_code} - {response.text}")
         
+        
 def delete_repo(current_repo_id):
-    url = f'{API_BASE_URL}/accounts/{DBT_CLOUD_ACCOUNT_ID}/projects/{DBT_CLOUD_PROJECT_ID}/repositories/{current_repo_id}/'
+    url = f"{API_BASE_URL}/accounts/{DBT_CLOUD_ACCOUNT_ID}/projects/{DBT_CLOUD_PROJECT_ID}/repositories/{current_repo_id}/"
     response = requests.delete(url, headers=HEADERS)
 
     if response.status_code == 200:
@@ -42,14 +43,14 @@ def delete_repo(current_repo_id):
     else:
         raise Exception(f"❌ Failed to unlink the repository: {response.status_code} - {response.text}")  
         
-def create_repository():
+def create_repository(new_github_repo):
     payload = {
         'account_id': DBT_CLOUD_ACCOUNT_ID,
         'project_id': DBT_CLOUD_PROJECT_ID,
         'remote_url': REMOTE_URL,
         'git_clone_strategy': 'github_app',
         'github_installation_id': GITHUB_INSTALLATION_ID,
-        'pull_request_url_template': f'https://github.com/{NEW_GIT_ORG_REPO}/compare/{{source}}...{{destination}}'
+        'pull_request_url_template': f'https://github.com/{new_github_repo}/compare/{{source}}...{{destination}}'
     }
     url = f"{API_BASE_URL}/accounts/{DBT_CLOUD_ACCOUNT_ID}/projects/{DBT_CLOUD_PROJECT_ID}/repositories/"
     response = requests.post(url, headers=HEADERS, json=payload)
@@ -78,9 +79,10 @@ def link_repository_to_project(project_name, repo_id):
 # === Main ===
 if __name__ == "__main__":
     try:
-        project_name, current_repo_id = get_current_project_info(DBT_CLOUD_PROJECT_ID)
+        project_name, current_repo_id, current_github_repo = get_current_project_info(DBT_CLOUD_PROJECT_ID)
         delete_repo(current_repo_id)
-        repo_id = create_repository()
+        new_github_repo = current_github_repo.replace(current_github_org, new_github_org)
+        repo_id = create_repository(new_github_repo)
         link_repository_to_project(project_name, repo_id)
     except Exception as e:
         print(str(e))
